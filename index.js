791d187159db355bc8fc85b79197d38702b57a67
AVIRAL SRIVASTAVA <43886946+Aviral00@users.noreply.github.com>
2020-07-24 13:01:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var x = Math.floor(Math.random()*6) + 1;
 var y = Math.floor(Math.random()*6) + 1;
-document.querySelector(".img1").setAttribute("src", "/home/aviral/Web Development Udemy/Dice Game/images/dice"+x+".png");
-document.querySelector(".img2").setAttribute("src", "/home/aviral/Web Development Udemy/Dice Game/images/dice"+y+".png");
+document.querySelector(".img1").setAttribute("src", "images/dice"+x+".png");
+document.querySelector(".img2").setAttribute("src", "images/dice"+y+".png");
 if (x > y){
 
   document.querySelector("h1").innerHTML = "🚩 Player1 Wins!"
